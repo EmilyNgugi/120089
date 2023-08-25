81030ce584284fde9aedfe44c6ecd7ae985bba8e
Emily Ngugi <72138870+EmilyNgugi@users.noreply.github.com>
2023-08-25 18:57:51 +0300

    Update log_user.php

--- log_user.php
+++ log_user.php
@@ -6,16 +6,16 @@ if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Hash the password
+    
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    // Prepare and execute the user registration (INSERT) query into the "signup" table
+    
     $stmt_insert = $conn->prepare("INSERT INTO signup (firstname, email, password) VALUES (?, ?, ?)");
     $stmt_insert->bind_param("sss", $username, $email, $hashedPassword);
     $stmt_insert->execute();
     $stmt_insert->close();
 
-    // Redirect to the home page after successful registration
+    
     header('Location: home.php');
     exit();
 }
@@ -24,7 +24,7 @@ if (isset($_POST['login_submit'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Prepare and execute the SELECT query to retrieve hashed password from the "signup" table
+    
     $stmt_select = $conn->prepare("SELECT password, username FROM signup WHERE email = ?");
     $stmt_select->bind_param("s", $email);
     $stmt_select->execute();
@@ -35,15 +35,15 @@ if (isset($_POST['login_submit'])) {
         $storedPassword = $row['password'];
         $username = $row['username'];
 
-        // Verify the password with the hashed password from the "signup" table
+        
         if (password_verify($password, $storedPassword)) {
-            // Prepare and execute the INSERT query to store login details in the "userlogin" table
+            
             $stmt_login = $conn->prepare("INSERT INTO userlogin (username, email) VALUES (?, ?)");
             $stmt_login->bind_param("ss", $username, $email);
             $stmt_login->execute();
             $stmt_login->close();
 
-            // Redirect to the home page after successful login
+            
             header('Location: home.php');
             exit();
         } else {
